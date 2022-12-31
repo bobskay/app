a08788be904b83e38edaedc64faf2e7e1a77228c
bobskay <744548762@qq.com>
2022-12-31 17:25:51 +0800

    init

--- html/vue.config.js
+++ html/vue.config.js
@@ -5,12 +5,12 @@ module.exports = defineConfig({
   publicPath:'/',
   devServer: {
     host: "localhost",
-    port: 8082, // 端口号
+    port: 9999, // 端口号
     https: false, // https:{type:Boolean}
     open: false, //配置自动启动浏览器
     proxy: {
       "/api": {
-        target: "http://47.74.53.129:9999",
+        target: "http://127.0.0.1:8888/",
         ws: false,// 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
